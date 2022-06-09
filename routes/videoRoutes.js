7e3cc0dbb12a020697d6d32630e7fff3ce56eab1
prudziejewski <p.rudziejewski@gmail.com>
2022-06-08 22:18:01 -0400

    Set up server

--- routes/videoRoutes.js
+++ routes/videoRoutes.js
@@ -18,6 +18,21 @@ fs.readFile('../data/videos.json', 'utf-8', (err, jsonString) => {
 }
 });
 
+const newVideoDetails = {
+    title: "This is a test",
+    channel: "Philip Rudz",
+    image: "https://i.imgur.com/i6S8m7I.jpg",
+    description: "This is a test to see if reading and writing files is working correctly!",
+    views: "13,092,284",
+    likes: "175,985",
+    duration: "4:20",
+    video: "https://project-2-api.herokuapp.com/stream",
+    timestamp: 1632344461000,
+};
+
+fs.writeFile()
+
+
 
 router.get("/", function (req, res) {
     res.send(videos);
